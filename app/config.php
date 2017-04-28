59b32c99a7f70760b6da6b8594d0461bcd210804
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-04-28 16:48:43 -0300

    config.php

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'displayErrorDetails' => true,
+    'database'            => [
+        'driver'   => 'pdo_mysql',
+        'host'     => 'localhost',
+        'dbname'   => 'cepremg',
+        'user'     => 'root',
+        'password' => '',
+    ],
+    'isDevMode'           => true,
+];
