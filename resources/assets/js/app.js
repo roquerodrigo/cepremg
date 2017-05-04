f6bc7937b74036bd058c04eb12b760efb2270a5f
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-05-03 22:22:52 -0300

    Tradução do Highcharts

---
+++ resources/assets/js/app.js
@@ -0,0 +1,22 @@
+Highcharts.setOptions({
+        lang: {
+            loading: 'Aguarde...',
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+            shortMonths: ['Jan', 'Feb', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            exportButtonTitle: "Exportar",
+            printButtonTitle: "Imprimir",
+            rangeSelectorFrom: "De",
+            rangeSelectorTo: "Até",
+            rangeSelectorZoom: "Periodo",
+            downloadPNG: 'Download imagem PNG',
+            downloadJPEG: 'Download imagem JPEG',
+            downloadPDF: 'Download documento PDF',
+            downloadSVG: 'Download imagem SVG'
+            // resetZoom: "Reset",
+            // resetZoomTitle: "Reset,
+            // thousandsSep: ".",
+            // decimalPoint: ','
+        }
+    }
+);
