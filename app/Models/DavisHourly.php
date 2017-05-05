13bdad01fbdba6b82917be01d2f5ea12cdee9691
Unknown <pedroprado010@gmail.com>
2017-05-04 22:01:48 -0300

    Added number of datas column
    added to monthly, daily and hourly tables

--- app/Models/DavisHourly.php
+++ app/Models/DavisHourly.php
@@ -7,5 +7,26 @@ namespace App\Models;
  **/
 class DavisHourly extends Davis
 {
+	/**
+     * @var int Número de dados estatísticos na coluna
+     * @Column(name="numberOfData", type="integer", nullable=false, options={"default":0})
+     */
+	private $numberOfData;
+	
+	/**
+	 *	@return int 
+	 */
+	public function getNumberOfData() {
+		return $numberOfData;
+	}
 
+	/**
+	 * @param int 
+	 *
+	 * @return DavisMonthly
+	 */
+	public function setNumberOfData(int $numberOfData) {
+		$this->numberOfData = $numberOfData;
+		return $this;
+	}
 }
