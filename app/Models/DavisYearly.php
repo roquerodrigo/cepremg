32644822909d6b6e2125a997dea078996b18192c
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-05-07 14:17:49 -0300

    Atributo desnecessário

--- app/Models/DavisYearly.php
+++ app/Models/DavisYearly.php
@@ -7,26 +7,4 @@ namespace App\Models;
  **/
 class DavisYearly extends Davis
 {
-	/**
-     * @var int Número de dados estatísticos na coluna
-     * @Column(name="numberOfData", type="integer", nullable=false, options={"default":0})
-     */
-	private $numberOfData;
-	
-	/**
-	 *	@return int 
-	 */
-	public function getNumberOfData() {
-		return $numberOfData;
-	}
-
-	/**
-	 * @param int 
-	 *
-	 * @return DavisMonthly
-	 */
-	public function setNumberOfData(int $numberOfData) {
-		$this->numberOfData = $numberOfData;
-		return $this;
-	}
 }
