bd941981509dfec16c193a2aec829038b906ab8e
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-05-09 10:14:54 -0300

    Atualização dos graficos

--- app/Controllers/DataController.php
+++ app/Controllers/DataController.php
@@ -7,6 +7,7 @@ use App\Models\DavisHourly;
 use App\Models\DavisMonthly;
 use App\Models\DavisYearly;
 use Doctrine\ORM\QueryBuilder;
+use Ghunti\HighchartsPHP\Highchart;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -19,59 +20,81 @@ class DataController extends Controller
         $end = $args['end'];
         $timePeriod = $args['timePeriod'];
 
-        $chartType = 'spline';
-        $series = [];
+        $chart = new Highchart(Highchart::HIGHSTOCK);
+        $chart->xAxis->type = 'datetime';
+
+        $chart->chart = [
+            'height' => '500px',
+            'type'   => 'line',
+        ];
 
         switch ($type) {
             case 'temp':
-                $chartName = 'Temperatura';
-                $unidadeMedida = 'Temperatura (ºC)';
-                $series['hiTemp']['name'] = 'Temperatura máxima';
-                $series['lowTemp']['name'] = 'Temperatura mínima';
-                $series['tempOut']['name'] = 'Temperatura média';
+                $chart->title->text = 'Temperatura';
+                $chart->yAxis['title']['text'] = 'Temperatura (°C)';
+
+                $chart->series['hiTemp']->name = 'Temperatura máxima';
+                $chart->series['lowTemp']->name = 'Temperatura mínima';
+                $chart->series['tempOut']->name = 'Temperatura média';
+
                 break;
 
             case 'outHum':
-                $chartName = 'Umidade do ar';
-                $unidadeMedida = 'Umidade do ar (%)';
-                $series['outHum']['name'] = 'Umidade do ar';
+                $chart->title->text = 'Umidade do ar';
+                $chart->yAxis['title']['text'] = 'Umidade do ar (%)';
+
+                $chart->series['outHum']->name = 'Umidade do ar';
+
                 break;
 
             case 'dewPt':
-                $chartName = 'Temperatura de ponto de orvalho';
-                $unidadeMedida = 'Temperatura de ponto de orvalho (ºC)';
-                $series['dewPt']['name'] = 'Temperatura de ponto de orvalho';
+                $chart->title->text = 'Temperatura de ponto de orvalho';
+                $chart->yAxis['title']['text'] = 'Temperatura de ponto de orvalho (ºC)';
+
+                $chart->series['dewPt']->name = 'Temperatura de ponto de orvalho';
+
                 break;
 
             case 'windSpeed':
-                $chartName = 'Intensidade do vento';
-                $unidadeMedida = 'Intensidade do vento (km/h)';
-                $series['windSpeed']['name'] = 'Intensidade do vento';
+                $chart->title->text = 'Intensidade do vento';
+                $chart->yAxis['title']['text'] = 'Intensidade do vento (km/h)';
+
+                $chart->series['windSpeed']->name = 'Intensidade do vento';
+
                 break;
 
             case 'bar':
-                $chartName = 'Pressão atmosférica';
-                $unidadeMedida = 'Pressão atmosférica (hPa)';
-                $series['bar']['name'] = 'Pressão atmosférica';
+                $chart->title->text = 'Pressão atmosférica';
+                $chart->yAxis['title']['text'] = 'Pressão atmosférica (hPa)';
+
+                $chart->series['bar']->name = 'Pressão atmosférica';
+
                 break;
 
             case 'rain':
-                $chartName = 'Precipitação';
-                $unidadeMedida = 'Precipitação (mm)';
-                $series['rain']['name'] = 'Precipitação';
-                $chartType = 'bar';
+                $chart->title->text = 'Precipitação';
+                $chart->yAxis['title']['text'] = 'Precipitação (mm)';
+
+                $chart->chart['type'] = 'bar';
+
+                $chart->series['rain']->name = 'Precipitação';
+
                 break;
 
             case 'solarRad':
-                $chartName = 'Radiação solar';
-                $unidadeMedida = 'Radiação solar (W/m²)';
-                $series['solarRad']['name'] = 'Radiação solar';
+                $chart->title->text = 'Radiação solar';
+                $chart->yAxis['title']['text'] = 'Radiação solar (W/m²)';
+
+                $chart->series['solarRad']->name = 'Radiação solar';
+
                 break;
 
             case 'UVIndex':
-                $chartName = 'Índice ultra violeta';
-                $unidadeMedida = 'Índice ultra violeta (UV)';
-                $series['UVIndex']['name'] = 'Índice ultra violeta';
+                $chart->title->text = 'Índice ultra violeta';
+                $chart->yAxis['title']['text'] = 'Índice ultra violeta (UV)';
+
+                $chart->series['UVIndex']->name = 'Índice ultra violeta';
+
                 break;
             default:
                 return $response->withStatus(404);
@@ -113,40 +136,21 @@ class DataController extends Controller
         }
 
         $results = $qb->getQuery()->getArrayResult();
+
         foreach ($results as $result) {
             $dateTime = $result['dateTime']->format('U') * 1000;
 
             if ($type == 'temp') {
-                $series['hiTemp']['data'][] = [$dateTime, (float) $result['hiTemp']];
-                $series['lowTemp']['data'][] = [$dateTime, (float) $result['lowTemp']];
-                $series['tempOut']['data'][] = [$dateTime, (float) $result['tempOut']];
+                $chart->series['hiTemp']->data[] = [$dateTime, (float) $result['hiTemp']];
+                $chart->series['lowTemp']->data[] = [$dateTime, (float) $result['lowTemp']];
+                $chart->series['tempOut']->data[] = [$dateTime, (float) $result['tempOut']];
             } else {
-                $series[$type]['data'][] = [$dateTime, (float) $result[$type]];
+                $chart->series[$type]->data[] = [$dateTime, (float) $result[$type]];
+                $chart->series[$type]->gapSize = 1;
             }
         }
 
-        return $response->withJson([
-            'chart'  => [
-                'height'       => 500,
-                'type'         => $chartType,
-                'connectNulls' => false,
-            ],
-            'series' => $series,
-            'title'  => [
-                'text' => $chartName,
-            ],
-            'xAxis'  => [
-                'type'  => 'datetime',
-                'title' => [
-                    'text' => 'Data',
-                ],
-            ],
-            'yAxis'  => [
-                'title' => [
-                    'text' => $unidadeMedida,
-                ],
-            ],
-        ]);
+        return $chart->renderOptions();
     }
 
     public function index(Request $request, Response $response, array $args)
